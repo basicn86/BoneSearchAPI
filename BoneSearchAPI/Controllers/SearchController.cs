92458bbf611f169fe8fcfa8bb84dcb3a7dfcbd8f
Nedim Basic <basicn86@gmail.com>
2023-08-07 05:18:51 -0400

    Add project files.

---
+++ BoneSearchAPI/Controllers/SearchController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoneSearchAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        [HttpGet]
+        //get method
+        public string Get()
+        {
+            return "Testing";
+        }
+    }
+}
