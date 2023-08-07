35f7ab563c6cf6fee49884baaf58a5a057afd570
Nedim Basic <basicn86@gmail.com>
2023-08-07 09:10:29 -0400

    v1 prototype

--- BoneSearchAPI/Controllers/SearchController.cs
+++ BoneSearchAPI/Controllers/SearchController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
 
 namespace BoneSearchAPI.Controllers
 {
@@ -8,10 +11,144 @@ namespace BoneSearchAPI.Controllers
     public class SearchController : ControllerBase
     {
         [HttpGet]
-        //get method
-        public string Get()
+        public IEnumerable<SearchResult> Get(string terms)
         {
-            return "Testing";
+            //throw an error if terms is null
+            if (terms == null)
+            {
+                throw new ArgumentNullException(nameof(terms));
+            }
+
+            //if terms is empty, throw another error
+            if (terms == string.Empty)
+            {
+                throw new ArgumentException("Terms cannot be empty", nameof(terms));
+            }
+
+            List<int> wordID = ConvertStringToIDs(terms);
+            Dictionary<int, int> wordRelevance = GetWordRelevance(wordID);
+            List<SearchResult> searchResults = ConvertPageIDsToSearchResults(wordRelevance);
+
+            //set the response type to application/json
+            Response.ContentType = "application/json";
+
+            return searchResults;
+        }
+        
+        private List<SearchResult> ConvertPageIDsToSearchResults(Dictionary<int, int> pageIDs)
+        {
+            List<SearchResult> result = new List<SearchResult>();
+            using MySqlConnection con = new MySqlConnection("server=localhost;database=searchv2;user=root;password=");
+            con.Open();
+
+
+            //loop through each pageID in the dictionary
+            foreach (KeyValuePair<int, int> entry in pageIDs)
+            {
+                //create mysqlcommand object
+                MySqlCommand cmd = new MySqlCommand("SELECT domain.name, domain.https, path, title FROM page JOIN domain ON page.domain_id = domain.id WHERE page.id=@page_id;", con);
+
+                //bind the parameter
+                cmd.Parameters.AddWithValue("@page_id", entry.Key);
+
+                //execute the query
+                var reader = cmd.ExecuteReader();
+
+                //read the result
+                while (reader.Read())
+                {
+                    SearchResult searchResult = new SearchResult();
+                    searchResult.title = reader.GetString("title");
+                    searchResult.url = reader.GetString("path");
+                    
+                    result.Add(searchResult);
+                }
+
+                //close the reader
+                reader.Close();
+
+                //if the results are greater than 10, break out of the loop
+                if (result.Count > 10)
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        //Get word relevance from list of word IDs
+        private Dictionary<int, int> GetWordRelevance(List<int> wordID)
+        {
+            Dictionary<int, int> wordRelevance = new Dictionary<int, int>();
+
+            using MySqlConnection con = new MySqlConnection("server=localhost;database=searchv2;user=root;password=");
+            con.Open();
+
+            foreach (int word in wordID)
+            {
+                //create mysqlcommand object
+                MySqlCommand cmd = new MySqlCommand("SELECT page_id, score FROM word_relevance WHERE word_id=@word_id ORDER BY score DESC", con);
+
+                //bind the parameter
+                cmd.Parameters.AddWithValue("@word_id", word);
+
+                //execute the query
+                var reader = cmd.ExecuteReader();
+
+                //read the result
+                while (reader.Read())
+                {
+                    int page_id = reader.GetInt32("page_id");
+                    int score = reader.GetInt32("score");
+
+                    if (wordRelevance.ContainsKey(page_id))
+                    {
+                        wordRelevance[page_id] += score;
+                    }else
+                    {
+                        wordRelevance.Add(page_id, score);
+                    }
+                }
+
+                //close the reader
+                reader.Close();
+            }
+
+            return wordRelevance;
+        }
+       
+        private List<int> ConvertStringToIDs(string _input)
+        {
+            string[] words = _input.Split(' ');
+            List<int> wordID = new List<int>();
+
+            using MySqlConnection con = new MySqlConnection("server=localhost;database=searchv2;user=root;password=");
+            con.Open();
+
+            foreach (string word in words)
+            {
+                //create mysqlcommand object
+                MySqlCommand cmd = new MySqlCommand("SELECT id FROM word WHERE word.word = @word", con);
+
+                //bind the parameter
+                cmd.Parameters.AddWithValue("@word", word);
+
+                //execute the query
+                var reader = cmd.ExecuteReader();
+
+                //read the result
+                while (reader.Read())
+                {
+                    wordID.Add(reader.GetInt32(0));
+                }
+
+                reader.Close();
+            }
+
+            con.Close();
+
+            return wordID;
         }
     }
 }
---
+++ BoneSearchAPI/SearchResult.cs
@@ -0,0 +1,8 @@
+﻿namespace BoneSearchAPI
+{
+    public class SearchResult
+    {
+        public string title { get; set; }
+        public string url { get; set; }
+    }
+}
